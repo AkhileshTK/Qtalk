c22bd3a6e73e84f01075fc596ac35b801b65cba2
AkhileshTK <akhileshtk11@gamil.com>
2024-11-06 09:59:26 +0530

    2nd commit

--- qtalk/src/main/java/com/qsp/App.java
+++ qtalk/src/main/java/com/qsp/App.java
@@ -6,5 +6,7 @@ package com.qsp;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Second change");
+
     }
 }
